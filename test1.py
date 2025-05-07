e9aa31e771ef8eff088a9dc9c394f40294fbf95d
이은채 <25_49@bssm.hs.kr>
2025-05-07 20:18:32 +0900

    tab2

--- test1.py
+++ test1.py
@@ -9,7 +9,11 @@ col1, col2 = st.columns([2,3])
 tab1, tab2 = st.tabs(['Tab A', 'Tab B'])
 with col1 :
     st.title("here is column1 title")
-
+    with tab1 :
+        st.write('hello')
+    with tab2 :
+        st.write("hi")
+        
 with col2 :
     st.title("here is column2 title")
     st.checkbox("this is checkbox1 in col2")
